1c1e69c69a897b191ec3ef3abf1cb2fe1e6cc623
Gustavo <rochatambelini@gmail.com>
2019-05-06 05:14:35 -0300

    Navbar e cover

--- index.php
+++ index.php
@@ -4,37 +4,40 @@
     <meta charset="utf-8" />
     <title>Gustavo Rocha Tambelini</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">    
 </head>
 <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top navbar-expand-lg navbar-light shadow-lg p-3 mb-5 rounded" style="position: absolute; width:100%;">
-        <div class="container">
+    <header>
+        <nav class="navbar navbar-inverse navbar-fixed-top navbar-expand-lg navbar-light shadow-lg p-3 mb-5 rounded" style="position: absolute; width:100%;">
+            <div class="container">
 
-        
-            <a class="navbar-brand" href="#">LOGO</a>
+                <a class="navbar-brand" href="#">LOGO</a>
 
-            <button class="navbar-toggler" data-toggler="collapse" data-target="#navbarMenu">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                <button class="navbar-toggler" data-toggler="collapse" data-target="#navbarMenu">
 
-            <div class="navbar-collapse collapse" id="navbarMenu">
-                <div class="navbar-nav ">
-                    <a class="nav-item nav-link text-primary active" href="#">HOME</a>
-                    <a class="nav-item nav-link" href="#">PROFICIONAL</a>
-                    <a class="nav-item nav-link" href="#">ESPERIENCIA</a>
-                    <a class="nav-item nav-link" href="#">PORTFOLIO</a>
-                    <a class="nav-item nav-link" href="#">CONTATO</a>
+                     <span class="navbar-toggler-icon">
+                     </span>
+
+                </button>
+
+                <div class="navbar-collapse collapse" id="navbarMenu">
+
+                    <div class="navbar-nav ">
+
+                        <a class="nav-item nav-link text-primary active" href="#">HOME</a>
+                        <a class="nav-item nav-link" href="#">PROFICIONAL</a>
+                        <a class="nav-item nav-link" href="#">ESPERIENCIA</a>
+                        <a class="nav-item nav-link" href="#">PORTFOLIO</a>
+                        <a class="nav-item nav-link" href="#">CONTATO</a>
+
+                    </div>
+
                 </div>
+
             </div>
-         </div>
-    </nav>
-    
-    <div class="container-home">
-        <div class="container-home-smart">
-              
-        </div>
-    
-    </div>
+
+        </nav>                  
+
 <script type="text/javascript" src="assets/js/jquery.min.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>   
 </body>
